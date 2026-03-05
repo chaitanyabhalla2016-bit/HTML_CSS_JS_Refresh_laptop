8293d11b16b0cc5142f5fcb9dce03093b035248e
cbhallamudi05 <124886639+cbhallamudi05@users.noreply.github.com>
2026-03-05 20:34:07 +0530

    Added data set to stats

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     let tasksStats = {
         total: 15,
-        completed: 9,
-        remaining: 6
+        completed: 10,
+        remaining: 5
     };
+
+    const progress = () => Math.round(((tasksStats["total"] - tasksStats["remaining"]) / tasksStats["total"])*100);
 
     const taskStatsLists = document.querySelectorAll('.hero-tasks-stat');
     taskStatsLists.forEach(el =>{
         // console.log(el.dataset.taskStat);
         // step1: el.textContent = el.dataset.taskStat;
-        el.textContent = tasksStats[el.dataset.taskStat];
-
+        const statKey = el.dataset.taskStat;
+        el.textContent = statKey === "progress"?`Completion progress: ${progress()}%` :`${statKey} tasks ${tasksStats[statKey]}`;
     });
+    const progressBar = document.querySelector('.progress-bar');
+    progressBar.dataset.taskProgress = progress();
+    progressBar.style.width = progress()+"%";
 });
