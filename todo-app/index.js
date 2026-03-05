5b682eb6d434e529d33be35ad067098db9606066
cbhallamudi05 <cbhallamudi05@gmail.com>
2026-03-05 11:05:58 +0530

    till dynamic stats in header

--- todo-app/index.js
+++ todo-app/index.js
@@ -4,4 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
         completed: 9,
         remaining: 6
     };
+
+    const taskStatsLists = document.querySelectorAll('.hero-tasks-stat');
+    taskStatsLists.forEach(el =>{
+        // console.log(el.dataset.taskStat);
+        // step1: el.textContent = el.dataset.taskStat;
+        el.textContent = tasksStats[el.dataset.taskStat];
+
+    });
 });
